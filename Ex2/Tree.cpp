34908ab2c2b2e66e2d27d6acdf310d70e10b128b
PAZJR93 <omerpaz93@gmail.com>
2019-03-15 13:36:18 +0200

    update all functions

--- Ex2/Tree.cpp
+++ Ex2/Tree.cpp
@@ -4,20 +4,73 @@
 
 using namespace std;
 using namespace ariel;
-
-    Tree::Tree(){
-    }
-
     Node* root;
 
-    void Tree::insert(int x){
-    root = fun::insert(x,root);
-     }
-    void Tree::disp() { 
-    fun::display(root, 10);
-    }
 
-////// private methods ///////
+                                            // constractor //
+    Tree::Tree()
+    {
+    root = NULL;
+    }
+                                            // distractor //   
+    Tree::~Tree()
+    {
+        root = fun::makeEmpty(root);
+    }
+                                        
+
+
+
+                                        ////// public methods ///////
+
+ 	void Tree::insert(int x){
+		root = fun::insert(x,root);
+	}
+	void Tree::disp() { 
+		fun::display(root, 10);
+	}
+	int Tree::parent (int x){
+		Node* temp =fun::parent(root,x);
+		if (temp==NULL) {
+			cout <<"No Parent found"<<endl;
+			return -1;
+		}
+		else return temp->data;
+	}
+	int Tree::left(int x){
+		return fun::left(x,root);
+	}
+
+	int Tree::right(int x){
+		return fun::right(x,root);
+	}
+	int Tree::size(){
+		return fun::size(root);
+	}
+
+	int Tree::root_return(){
+		return root->data;
+	}
+
+	bool Tree::contains(int x)
+	{
+		Node* temp = fun::find(root, x);
+		if(temp==NULL)return false;
+		else 
+			return true ;
+	}
+
+	bool Tree::remove(int x)
+	{
+		Node* temp = fun::remove(x, root);
+		if(temp == NULL) return false;
+		else return true;
+	}
+
+                                        ////// end public methods ///////
+
+
+                                        ////// private methods ///////
 
     void fun::display(Node* p, int indent) {
 
@@ -37,28 +90,161 @@ using namespace ariel;
   }
 }
 
-// insert function 
-        Node* fun::insert(int x, Node* p){
-
-            if(p == NULL)
+// empty the tree //
+ Node* fun::makeEmpty(Node* t)
+    {
+        if(t == NULL)
+            return NULL;
         {
-            p = new Node;
-            p->data = x;
-            p->left = p->right = NULL;
+            fun::makeEmpty(t->left);
+            fun::makeEmpty(t->right);
+            delete t;
         }
+        return NULL;
+    }
+    Node* fun::remove(int x, Node* t)
+{
+	Node* temp;
+	if(t == NULL)
+		return NULL;
+	else if(x < t->data)
+		t->left = fun::remove(x, t->left);
+		else if(x > t->data)
+			t->right = fun::remove(x, t->right);
+			else if(t->left && t->right)
+			{
+				temp = fun::findMin(t->right);
+				t->data = temp->data;
+				t->right = fun::remove(t->data, t->right);
+			}
+			else
+			{
+				temp = t;
+				if(t->left == NULL)
+					t = t->right;
+					else if(t->right == NULL)
+						t = t->left;
+						delete temp;
+			}
 
-        else if(x < p->data){
-            p->left = fun::insert(x, p->left);
+	return t;
+}
 
-        }
+Node* fun::findMin(Node* t)
+{
+	if(t == NULL)
+		return NULL;
+	else if(t->left == NULL)
+		return t;
+	else
+		return fun::findMin(t->left);
+}
 
-        if(x > p->data)
-            p->right = fun::insert(x, p->right);
-    
-        return p;
-        }
 
-///////// end ////////////
+Node* fun::insert(int x, Node* p){
+
+	if(p == NULL)
+	{
+		p = new Node;
+		p->data = x;
+		p->left = p->right = NULL;
+	}
+
+	else if(x < p->data){
+		p->left = fun::insert(x, p->left);
+
+	}
+
+	if(x > p->data)
+		p->right = fun::insert(x, p->right);
+
+		return p;
+}
+
+
+Node* fun::parent(Node* root, int n) {
+	if (root == NULL)
+		return NULL;
+	else
+		if ( (root->left!=NULL && root->left->data == n) || (root->right!=NULL) && (root->right->data == n)){
+			return root;
+		}
+		else {
+			Node* result= fun::parent(root->left, n);
+			if (result!=NULL){
+				return result;
+			}
+			else{
+				return fun::parent(root->right, n);
+			}
+		}
+
+}
+int fun::left(int x ,Node* root){
+
+    if(x == root->data){
+       if(root->left == NULL){
+           cout << "No Left Child for"<< x << endl;
+           return -1;
+       }
+           else {
+           return root->left->data;
+         }
+    }
+    if(x < root->data){
+        fun::left(x,root->left);
+    }
+    else if(x > root->data){
+        fun::left(x,root->right);
+    }
+}
+
+int fun::right(int x ,Node* root){
+
+    if(x == root->data){
+       if(root->right == NULL){
+           cout << "No Right Child for"<< x << endl;
+           return -1;
+       }
+           else {
+           return root->right->data;
+         }
+    }
+    if(x < root->data){
+        fun::right(x,root->left);
+    }
+    else if(x > root->data){
+        fun::right(x,root->right);
+    }
+}
+
+int fun::size(Node* root)  
+{  
+	if (root == NULL)  
+		return 0;  
+	else
+		return(fun::size(root->left) + 1 + fun::size(root->right));  
+}
+
+Node* fun::find(Node* t, int x)
+{
+	if(t == NULL)
+		return NULL;
+	else if(x < t->data)
+		return fun::find(t->left, x);
+	else if(x > t->data)
+		return fun::find(t->right, x);
+	else
+		return t;
+}
+
+
+
+
+
+
+                                        ////// end  private methods ///////
+
 
 int main(){
     Tree t;
@@ -71,6 +257,13 @@ int main(){
 
 
     t.disp();
+    t.size();
+    t.remove(3);
+    t.disp();
+    cout <<"left of 4 is: " << t.left(4)<<endl;
+    cout <<"size is: " << t.size()<<endl;
+    cout <<"right of 4 is: " << t.right(4)<<endl;
+
 
     return 0;
 }
--- Ex2/Tree.hpp
+++ Ex2/Tree.hpp
@@ -1,27 +1,45 @@
 #include <iostream>
 using namespace std;
 
-    struct Node
-    {
-        int data;
-        Node* left;
-        Node* right;
-    };
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
 
 
 namespace ariel{
 
-    class Tree{
-        public:
-        Tree();
-        void insert(int);
-        void disp();
-
-    };
+	class Tree{
+		public:
+			Tree();
+	~Tree();
+	void insert(int);
+	void disp();
+	int parent (int x);
+	int right(int x);
+	int left(int x);
+	int size();
+	int root_return();
+	bool contains(int x);
+	bool remove(int x);
+	};
 }
 namespace fun{
 
-    Node* insert(int,Node*);
-    void display(Node*,int);
+	Node* insert(int,Node*);
+	void display(Node*,int);
+	Node* parent(Node* root, int n);
+	int right(int x ,Node* root);
+	int left(int x ,Node* root);
+	int size(Node* root);
+	Node* find(Node* t, int x);
+	Node* remove(int x, Node* t);
+	Node* makeEmpty(Node*);
+    Node* findMin(Node* t);
+
+
+
 
 }
